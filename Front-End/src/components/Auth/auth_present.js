37434560b24be1012e8a7fc5f01dad1fb9d859a1
Arnav <arnav27ahuja@gmail.com>
2024-12-04 13:39:25 +0530

    Added support for cookie saving

--- Front-End/src/components/Auth/auth_present.js
+++ Front-End/src/components/Auth/auth_present.js
@@ -2,13 +2,40 @@ const isTokenExpired = async(token) => {
     try {
         const response = await fetch('http://localhost/api/protected', {
             method:"POST",
-            headers: { 'Content-Type': 'application/json' },
+            credentials: "include",
+            headers: [
+              ["Content-Type", "application/json"],
+              ["Content-Type", "text/plain"]
+            ],
             body: JSON.stringify({"token":token})
         }
     ).then(response => response.json()).then() 
         const isAuth=response.Auth;
         if(isAuth) return true;
-        else return false
+        else {
+          const response = await fetch('http://localhost/api/refresh', {
+            method:"POST",
+            credentials: "include",
+            headers: [
+              ["Content-Type", "application/json"],
+              ["Content-Type", "text/plain"]
+            ],
+            body: JSON.stringify({"token":token})
+        }
+    ).then(response => response.json()).then() 
+    const access_token = response.token;
+    console.log(response);
+    localStorage.removeItem('token');
+    localStorage.setItem('token', access_token);
+    if(access_token){
+      isAuthenticated();
+      window.location.reload();
+      return true;
+    }
+    else{
+      return false;
+    }
+        }
       } catch (error) {
         console.error(error);
       }
