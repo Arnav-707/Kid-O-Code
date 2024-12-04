685de96db40b7cd9d2b6d4d426a7ee1acbf1f31c
Arnav <arnav27ahuja@gmail.com>
2024-12-04 13:43:50 +0530

    Seperate login and signup functionality

--- Front-End/src/components/Login/login.jsx
+++ Front-End/src/components/Login/login.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import './login.css';
-import {login} from '../Auth/Auth'
+import {login} from '../Auth/login'
+import { isAuthenticated } from "../Auth/auth_present";
 function LoginForm() {
     const [email, setEmail] = useState('');
     const [password, setPass] = useState('');
+    if(!isAuthenticated()){
     return (
         <div id ="main">
             <div id="naam">
                 <h1>Login Form</h1>
                 <form>
-                    <input value={email} type="text" onChange={(e) => setEmail(e.target.value)} placeholder="Username" required /><br />
+                    <input value={email} type="text" onChange={(e) => setEmail(e.target.value)} placeholder="Email" required /><br />
                     <input value={password} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Password" required /><br />
                     <input className="btn" type="button" value="Login" onClick={()=>login(email,password)}  />
                     <input className="btn" type="reset" value="Reset" />
@@ -18,7 +20,11 @@ function LoginForm() {
                 </form>
             </div>
         </div>
-    );
+        );
+    }
+    else{
+        window.location.href='/homepage';
+    }
 }
 
 export default LoginForm;
