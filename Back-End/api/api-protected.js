81c3b2de5c3eca30f0901306620fd669f2379194
Arnav <arnav27ahuja@gmail.com>
2024-12-04 13:30:20 +0530

    Added CORS, refresh path for token and divided functions into modules

--- Back-End/api/api-protected.js
+++ Back-End/api/api-protected.js
@@ -4,11 +4,12 @@ const bcrypt = require("bcrypt");
 const verifyToken = require('./auth');
 const mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
 let dotenv = require('dotenv').config();
 const url = dotenv.parsed.url;
-// const leaderboardSchema = require('../schema/Leaderboard.Schema');
-
+const {create_JWT, refresh_JWT} = require('./jwt_gen');
+const {send_mail} = require("./node-mail");
+const cookieparser = require('cookie-parser');
+const { assign } = require("nodemailer/lib/shared");
 //Database connection
 mongoose.connect(url)
     .then(() => {
@@ -21,37 +22,61 @@ var db = mongoose.connection;
 
 // Express Initialization
 const app = express();
-app.use(cors());
+app.use(cookieparser());
+app.use(cors( {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }));
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true})) 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header('Content-Type', 'application/json;charset=UTF-8')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+  })
 
-const secretkey = dotenv.parsed.secretkey;
 //Routes
-app.post("/signup",async (req,res)=>{
-    try{
-        const {name,email,password}= req.body;
-        const user = await db.collection('Users').findOne({email:Email});
+app.post("/signup", async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+        const user = await db.collection('Users').findOne({ email: email });
         const saltRounds = 10;
-        
+
         const hash_password = await bcrypt.hash(password, saltRounds)
 
         const data_user = {
             "name": name,
             "email": email,
             "password": hash_password
+            // ,"unique_number": unique_number
         }
-        if(!user){
-            db.collection('Users').insertOne(data_user,function(err, collection){ 
-                if (err) throw err; 
-                console.log("Record inserted Successfully"); 
-                console.log(data_user);   
+        if (!user) {
+            db.collection('Users').insertOne(data_user, function (err) {
+                if (err) throw err;
+                console.log("Record inserted Successfully");
+                console.log(data_user);
             });
-            token = jwt.sign({email},secretkey,{expiresIn:'1h'})
-    res.json({token}); 
+            send_mail(name);
+            const {access_token,refresh_token}= create_JWT(data_user);
+            res.cookie('jwt', refresh_token,{
+                httpOnly: true,
+                sameSite: 'None' , secure: true,
+                maxAge:24*60*60*1000
+            },()=>{
+                console.log("Cookie created")
+            });
+            next();
+            res.json({ 'token': access_token });
         }
-            else{
-            }
+        else {
+            return res.status(406).json({message:"Invalid Creds"});
+        }
     }
     catch (err) {
         console.error(err);
@@ -59,62 +84,96 @@ app.post("/signup",async (req,res)=>{
     }
 })
 
-app.post('/login',async (req,res)=>{
-    try{
-        const {email,password} = req.body;
-        const user = await db.collection('Users').findOne({email:email});
-        if(user){
+app.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const data_user ={
+            "email":email,
+            "password":password
+        }
+        const user = await db.collection('Users').findOne({ email: email });
+        if (user) {
             const pword_check = await bcrypt.compare(password, user.password);
-            if(pword_check){
-                token = jwt.sign({email},secretkey,{expiresIn:'1h'});
-                // console.log(token);
-                res.json({token});
+            if (pword_check) {
+                const {access_token,refresh_token}= create_JWT(data_user);
+                res.cookie('jwt', refresh_token,{
+                    httpOnly: false,
+                    sameSite: 'None' , secure: true,
+                    maxAge:24*60*60*1000
+                },()=>{
+                    console.log("Cookie created")
+                });
+                console.log(access_token);
+                res.json({ 'token': access_token });;
             }
-            else{
-                res.json({'message':"Invalid User"}); 
+            else {
+                res.json({ 'message': "Invalid User" });
             }
         }
     }
+    catch (err) {
+        console.log(err);
+    }
+})
+
+app.post('/refresh',async (req,res)=>{
+    try{
+    const refresh_token = req.cookies.jwt;
+    if(refresh_token){
+        const {access_token} = await refresh_JWT(refresh_token);
+        if(access_token){
+            res.json({'token':access_token});
+        }
+        else{
+            res.status(506).json({error:'Internal Server Error'});
+        }
+    }
+    else{
+        return false;
+    }}
     catch(err){
         console.log(err);
     }
 })
-app.post('/protected', verifyToken ,async (req,res)=>{
-    try{
-        const {token} = JSON.stringify(req.token);
-        console.log(typeof(token))
+
+app.post('/protected', async (req, res) => {
+    try {
+        const token = req.body.token;
         const auth = verifyToken(token);
-        if(auth){
-            res.json({"Auth":true})
+        if (auth) {
+            res.json({ "Auth": true })
         }
         else {
-            res.json({"Auth":false});
+            res.json({ "Auth": false });
         }
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
 })
-app.get('/leaderboard', async (req, res) => {
-    try {
-      const users = await db.collection('leaderboard').find({}).toArray(); // Fetch data from the leaderboard collection
-      res.json(users);
+app.post('/leaderboard', async (req, res) => {
+    if(verifyToken(req.body.token)){try {
+        const users = await db.collection('leaderboard').find({}).sort({score:-1}).toArray(); // Fetch data from the leaderboard collection
+        res.json(users);
     } catch (err) {
-      console.error('Error fetching users:', err);
-      res.status(500).json({ error: 'Failed to fetch users' });
+        console.error('Error fetching users:', err);
+        res.status(500).json({ error: 'Failed to fetch users' });
+    }}
+    else{
+        res.status(403).json({message:"Not Auth"});
     }
-  });
+});
 
 // API route to fetch quiz questions
-  app.get('/quiz', async (req, res) => {
+app.get('/quiz', async (req, res) => {
     try {
-      const questions = await db.collection("Quizez").find().toArray();
-      res.json(questions);
+        const questions = await db.collection("quizez").find().toArray();
+        res.json(questions);
     } catch (err) {
-      console.error('Error fetching quiz questions:', err);
-      res.status(500).json({ error: 'Failed to fetch quiz questions' });
+        console.error('Error fetching quiz questions:', err);
+        res.status(500).json({ error: 'Failed to fetch quiz questions' });
     }
-  });
-  
+});
+
 
 module.exports = app;
