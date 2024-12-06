5176057b7b70da7dac63d8b4bde38f8bcdd114f5
Arnav <arnav27ahuja@gmail.com>
2024-12-06 12:34:57 +0530

    Optimized signup function

--- Front-End/src/components/Auth/signup.js
+++ Front-End/src/components/Auth/signup.js
@@ -1,19 +1,21 @@
 const signup = async (email, password, name) => {
-    try{
+    if(name && password && email){try{
         const response = await fetch('http://localhost/api/signup', {
                 method:"POST",
-                mode: 'same-origin',
-                redirect: 'follow',
                 credentials: "include",
-                headers: { 'Content-Type': 'application/json','Accept': 'application/json' },
+                headers: [
+                  ["Content-Type", "application/json"],
+                  ["Content-Type", "text/plain"]
+                ],
                 body: JSON.stringify({ "name":name,"email":email, "password":password})
             }
         ).then(response => response.json())
+        console.log(response.token)
         localStorage.setItem('token', response.token);
         window.location.href='/homepage';
     } catch (error) {
       console.error(error);
-    };
+    };}
     return false;
 };
 
