000c2c4d39abd94ab355ffbfc829fe6bf3a8ea2a
Arnav <arnav27ahuja@gmail.com>
2024-12-06 12:41:41 +0530

    Fixed Login and Signup page and CSS

--- Front-End/src/components/Login/login.jsx
+++ Front-End/src/components/Login/login.jsx
@@ -11,9 +11,9 @@ function LoginForm() {
                 <form>
                     <input value={email} type="text" onChange={(e) => setEmail(e.target.value)} placeholder="Email" required /><br />
                     <input value={password} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Password" required /><br />
-                    <input className="btn" type="button" value="Login" onClick={()=>login(email,password)}  />
-                    <input className="btn" type="reset" value="Reset" />
-                    {/* <p>Forgot <a href="#">Password</a> or <a href="#">Username</a></p> */}
+                    <input className="login_btn" type="button" value="Login" onClick={()=>login(email,password)}  />
+                    {/* <input className="btn" type="reset" value="Reset" /> */}
+                    <p className="login_p">Forgot <a href="#">Password</a> or <a href="/signup">New User</a></p>
 
                 </form>
             </div>
--- Front-End/src/components/Login/signup.jsx
+++ Front-End/src/components/Login/signup.jsx
@@ -11,11 +11,11 @@ function Signup () {
                 <h1>Signup Form</h1>
                 <form>
                     <input value={name} type="text" onChange={(e) => setName(e.target.value)} placeholder="Name" required /><br />
-                    <input value={email} type="text" onChange={(e) => setEmail(e.target.value)} placeholder="Email" required /><br />
+                    <input value={email} type='email' onChange={(e) => setEmail(e.target.value)} placeholder="Email" required /><br />
                     <input value={password} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Password" required /><br />
-                    <input className="btn" type="button" value="Signup" onClick={()=>signup(email,password,name)}  />
-                    <input className="btn" type="reset" value="Reset" />
-                    {/* <p>Forgot <a href="#">Password</a> or <a href="#">Username</a></p> */}
+                    <input className="login_btn" type="button" value="Signup" onClick={()=>signup(email,password,name)}  />
+                    {/* <input className="btn" type="reset" value="Reset" /> */}
+                    <p className="login_p">Forgot <a href="#">Password</a> or <a href="/login">Already a user</a></p>
 
                 </form>
             </div>
