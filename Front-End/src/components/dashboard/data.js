bad1de5beb6d9471e3349b0baab70bfbb9b61850
Arnav898 <122724332+Arnav898@users.noreply.github.com>
2024-12-09 18:12:51 +0530

    Made user dashboard

---
+++ Front-End/src/components/dashboard/data.js
@@ -0,0 +1,11 @@
+export const userData = {
+    name: "John Doe",
+    userID : "1234",
+    email: "john.doe@example.com",
+    joinedDate: "January 1, 2022",
+    progress: 6, // Progress percentage
+  };
+  
+  export const quizData = [
+    { name: "Quiz", score: 8, totalMarks: 10, attempted: true },
+  ];
